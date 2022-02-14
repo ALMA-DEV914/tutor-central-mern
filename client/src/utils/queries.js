8b31a1eeff050b3fc6aef66e721bd8d043cb3ee0
ALMA_DEV914 <aungonalna58@gamil.com>
2022-02-13 23:38:09 -0800

    add components and pages for header

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+    }
+  }
+`;

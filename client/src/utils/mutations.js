ecaf5c8a3de747eb6e9d1a4ad3f530c91a2787ac
tarekyou <tarekyoussef88@hotmail.com>
2022-02-21 22:57:59 -0800

    frontend queries and mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
@@ -10,16 +10,19 @@ export const LOGIN = gql`
     }
   }
 `;
+
 export const ADD_USER = gql`
   mutation addUser(
     $firstName: String!
     $lastName: String!
+    $username: String!
     $email: String!
     $password: String!
   ) {
     addUser(
       firstName: $firstName
       lastName: $lastName
+      username: $username
       email: $email
       password: $password
     ) {
@@ -27,6 +30,65 @@ export const ADD_USER = gql`
       user {
         _id
       }
+    }
+  }
+`;
+
+export const ADD_POST = gql`
+  mutation addPost($postText: String!) {
+    addPost(postText: $postText) {
+      _id
+      postText
+      createdAt
+      username
+      comments {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($commentText: String!) {
+    addPost(commentText: $commentText) {
+      _id
+      commentText
+      createdAt
+      username
+    }
+  }
+`;
+
+export const ADD_CATEGORY = gql`
+  mutation addCategory($name: String!) {
+    addCategory(name: $name) {
+      _id
+      name
+    }
+  }
+`;
+
+export const UPDATE_POST = gql`
+  mutation updatePost($postText: String!) {
+    updatePost(postText: $postText) {
+      _id
+      postText
+      createdAt
+      username
+      comments {
+        _id
+      }
+    }
+  }
+`;
+
+export const UPDATE_COMMENT = gql`
+  mutation updateComment($commentText: String!) {
+    updateComment(commentText: $commentText) {
+      _id
+      commentText
+      createdAt
+      username
     }
   }
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,10 +1,79 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
+
+export const QUERY_POSTS = gql`
+  query posts($username: String) {
+    posts(username: $username) {
+      _id
+      postText
+      createdAt
+      username
+      comments {
+        _id
+        createdAt
+        username
+        commentText
+      }
+    }
+  }
+`;
+export const QUERY_CATEGORIES = gql`
+  query categories {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_POST = gql`
+  query post($id: ID!) {
+    post(_id: $id) {
+      _id
+      postText
+      createdAt
+      username
+      comments {
+        _id
+        createdAt
+        username
+        commentText
+      }
+    }
+  }
+`;
 
 export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      posts {
+        _id
+        postText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
   {
-    user {
-      firstName
-      lastName
+    me {
+      _id
+      username
+      email
+      posts {
+        _id
+        postText
+        createdAt
+        comments {
+          _id
+          createdAt
+          commentText
+          username
+        }
+      }
     }
   }
 `;

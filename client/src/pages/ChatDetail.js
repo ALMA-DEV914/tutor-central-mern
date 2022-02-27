e5ff6c29c89755d1a9b414ec07f817c538cb7db5
tbellenger <tbellenger@gmail.com>
2022-02-27 11:29:47 -0800

    added formatting and functionality to the chat

--- client/src/pages/ChatDetail.js
+++ client/src/pages/ChatDetail.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Auth from "../utils/auth";
 import { useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_CHAT } from "../utils/queries";
+import { Card, Form, Button } from "react-bootstrap";
+import { ADD_MESSAGE } from "../utils/mutations";
 
 function ChatDetail() {
   const { id } = useParams();
+  const [formState, setFormState] = useState({
+    messageText: "",
+  });
+  const [addMessage] = useMutation(ADD_MESSAGE);
 
-  const { loading, data } = useQuery(QUERY_CHAT, { variables: { chatId: id } });
-  console.log(data);
+  const { loading, data, refetch } = useQuery(QUERY_CHAT, {
+    variables: { chatId: id },
+  });
 
-  // const user = data?.me ||  {};
-  // console.log(user);
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(id);
+    try {
+      const mutationResponse = await addMessage({
+        variables: {
+          chatId: id,
+          messageText: formState.messageText,
+        },
+      });
+      console.log(mutationResponse);
+      refetch();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.currentTarget;
+    setFormState({ ...formState, [name]: value });
+  };
+
+  const getUsername = (id) => {
+    if (data.chat.tutor._id === id) {
+      return data.chat.tutor.username;
+    } else {
+      return data.chat.student.username;
+    }
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -19,21 +53,47 @@ function ChatDetail() {
 
   return (
     <>
-      <div>
-        <h2>
-          Viewing Chat {data.chat._id} between {data.chat.tutor.username} and{" "}
-          {data.chat.student.username}
-        </h2>
-        <section>
-          {data.chat.messages.map((message) => {
-            return <p>{message.messageText}</p>;
-          })}
-        </section>
-      </div>
+      <Card className='my-3'>
+        <Card.Header>
+          <Card.Title>
+            Viewing Chat {data.chat._id} between {data.chat.tutor.username} and{" "}
+            {data.chat.student.username} on {data.chat.createdAt}
+          </Card.Title>
+        </Card.Header>
 
-      <div>
-        <div></div>
-      </div>
+        <Card.Body>
+          <Form onSubmit={handleFormSubmit}>
+            <Form.Group className='mb-3'>
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                type='text'
+                name='messageText'
+                onChange={handleChange}
+              ></Form.Control>
+            </Form.Group>
+            <Button
+              className='mb-3'
+              variant='primary'
+              type='submit'
+              onClick={handleFormSubmit}
+            >
+              Send
+            </Button>
+          </Form>
+          {data.chat.messages.map((message, index) => {
+            return (
+              <Card key={index} className='mb-3'>
+                <Card.Body>
+                  <p>Date: {message.createdAt}</p>
+                  <p>From: {getUsername(message.from._id)}</p>
+                  <p>To: {getUsername(message.to._id)}</p>
+                  <p>Message: {message.messageText}</p>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </Card.Body>
+      </Card>
     </>
   );
 }

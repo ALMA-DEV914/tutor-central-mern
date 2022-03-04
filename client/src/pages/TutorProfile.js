af410a5bcf9711c3cfdefa07d25cb581e30afebd
tbellenger <tbellenger@gmail.com>
2022-03-04 15:22:20 -0800

    added photo upload

--- client/src/pages/TutorProfile.js
+++ client/src/pages/TutorProfile.js
@@ -4,16 +4,16 @@ import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_ME } from "../utils/queries";
 // import { useNavigate } from "react-router-dom";
 //import { useParams } from "react-router-dom";
-import { UPDATE_USER} from "../utils/mutations";
-import { Button, Card, Col, Form, Row, Alert} from 'react-bootstrap'
-import tutorProfile from '../assets/tutor.jpeg'
+import { UPDATE_USER } from "../utils/mutations";
+import { Button, Card, Col, Form, Row, Alert } from "react-bootstrap";
+import tutorProfile from "../assets/tutor.jpeg";
 
 function TutorProfile() {
   // return <div>Profile</div>;
   // let navigate = useNavigate();
 
   const { loading, data } = useQuery(QUERY_ME);
-  
+
   // console.log(data.me.user.username);
   // const [errorMessage, setErrorMessage] = useState("");
   const [formState, setFormState] = useState({
@@ -22,8 +22,6 @@ function TutorProfile() {
     // password: `${data.me.user.password}`,
     username: ``,
     password: ``,
-
-
   });
 
   const [updateUser] = useMutation(UPDATE_USER);
@@ -42,7 +40,6 @@ function TutorProfile() {
       setFormState({
         // ...formState,
         formState: { ...data.me.user },
-      
       });
       // return { ...data.me.user };
     }
@@ -50,7 +47,6 @@ function TutorProfile() {
     setFormState({
       ...formState,
       [name]: value,
-      
     });
   };
 
@@ -64,14 +60,14 @@ function TutorProfile() {
       const { data } = await updateUser({
         variables: { ...formState },
       });
-      
+
       // console.log(data);
       Auth.loggedIn(data.updateUser.token);
     } catch (e) {
       console.error(e);
     }
   };
-  
+
   // const user = data?.me ||  {};
   // console.log(user);
 
@@ -81,89 +77,98 @@ function TutorProfile() {
 
   function AlertDismissibleExample() {
     const [show, setShow] = useState(true);
-  
+
     if (show) {
       return (
-        <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+        <Alert variant='danger' onClose={() => setShow(false)} dismissible>
           <Alert.Heading>Hey! You got a new message!</Alert.Heading>
-          
-            Viewing Chat {data.chat} between {data.chat} and {" "}
-            {data.messageText} on {data.createdAt}
-    
-        
-          </Alert>
+          Viewing Chat {data.chat} between {data.chat} and {data.messageText} on{" "}
+          {data.createdAt}
+        </Alert>
       );
     }
     return <Button onClick={() => setShow(true)}>Show Messages</Button>;
   }
 
-
-  
   return (
     <>
-    <Row className="mt-4">
-      <Col>
-     <img  src={tutorProfile} alt="profile" style={{width: '400px', borderRadius:"100%", height: '400px'}}/>
-     </Col>
-   <Col className="mt-4">
-      <div>
-        <h2>Your Dashboard</h2>
-      </div>
+      <Row className='mt-4'>
+        <Col>
+          <img
+            src={data.me.user.photo}
+            alt='profile'
+            style={{ width: "400px", borderRadius: "100%", height: "400px" }}
+          />
+        </Col>
+        <Col className='mt-4'>
+          <div>
+            <h2>Your Dashboard</h2>
+          </div>
 
-      <div>
-        <div>
-          <p><b>Name:</b> {data.me.user.username}</p>
-          <p><b>Email Address:</b> {data.me.user.email}</p>
-          <p><b>Hourly rate: $</b>{data.me.tutor.hourlyRate}</p>
-          <p><b>Expertises:</b> {data.me.tutor.knownSubjects}</p>
-          <p><b>Bio:</b> {data.me.tutor.bio}</p>
-         
-          {/* <p>{data.me.tutor._id}</p> */}
-        </div>
-      </div>
-      <div><h3>Messages</h3>
-      <AlertDismissibleExample />
-      </div>
-      <div><h3>Student Lists</h3></div>
-      </Col>
-      <Form.Group>
-        <Card className="p-4" style={{ width: '28rem' }}>
-          <h3>Update Username and Password</h3>
-        <form onSubmit={handleFormSubmit}>
-          <input
-            className="form-input m-2"
-            placeholder="username"
-            name="username"
-            type="username"
-            id="username"
-            value={formState.username}
-            onChange={handleChange}
-            
-          />
-          <input
-            className="form-input"
-            placeholder="******"
-            name="password"
-            type="password"
-            id="password"
-            value={formState.password}
-            onChange={handleChange}
-            
-          />
-          {/* {errorMessage && (
+          <div>
+            <div>
+              <p>
+                <b>Name:</b> {data.me.user.username}
+              </p>
+              <p>
+                <b>Email Address:</b> {data.me.user.email}
+              </p>
+              <p>
+                <b>Hourly rate: $</b>
+                {data.me.tutor.hourlyRate}
+              </p>
+              <p>
+                <b>Expertises:</b> {data.me.tutor.knownSubjects}
+              </p>
+              <p>
+                <b>Bio:</b> {data.me.tutor.bio}
+              </p>
+
+              {/* <p>{data.me.tutor._id}</p> */}
+            </div>
+          </div>
+          <div>
+            <h3>Messages</h3>
+            <AlertDismissibleExample />
+          </div>
+          <div>
+            <h3>Student Lists</h3>
+          </div>
+        </Col>
+        <Form.Group>
+          <Card className='p-4' style={{ width: "28rem" }}>
+            <h3>Update Username and Password</h3>
+            <form onSubmit={handleFormSubmit}>
+              <input
+                className='form-input m-2'
+                placeholder='username'
+                name='username'
+                type='username'
+                id='username'
+                value={formState.username}
+                onChange={handleChange}
+              />
+              <input
+                className='form-input'
+                placeholder='******'
+                name='password'
+                type='password'
+                id='password'
+                value={formState.password}
+                onChange={handleChange}
+              />
+              {/* {errorMessage && (
             <div>
               <p className="error-text">{errorMessage}</p>
             </div>
           )} */}
-          <Button className="btn d-block" variant="primary" type="submit">
-            Submit
-          </Button>
-        </form>
-        </Card>
+              <Button className='btn d-block' variant='primary' type='submit'>
+                Submit
+              </Button>
+            </form>
+          </Card>
         </Form.Group>
-        
       </Row>
-      
     </>
   );
 }

472c661414a3ef9ca63dfebcc8bd23aa1f026493
ALMA_DEV914 <aungonalna58@gamil.com>
2022-03-05 00:10:01 -0800

    add logo

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import logo from '../../assets/logo.jpeg';
 
 function Header() {
   return (
     <Navbar bg='primary p-4' variant='dark' collapseOnSelect expand='lg'>
       <Container fluid>
-        <Navbar.Brand href='/' style={{fontFamily: 'fantasy', fontSize: '28px', fontWeight: '600'}}>Tutor-Student Connect</Navbar.Brand>
+      <Navbar.Brand href='/' style={{fontFamily: 'fantasy', fontSize: '28px', fontWeight: '600'}}><img src={logo} alt="logo" style={{width: '100px'}}/>Tutor-Student Connect</Navbar.Brand>
         <Navbar.Toggle aria-controls='responsive-navbar' />
         <Navbar.Collapse id='responsive-navbar'>
           <Nav className='me-auto' style={{fontSize:'18px'}}>

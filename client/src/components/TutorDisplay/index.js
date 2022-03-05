d916e298c0dd772e7f20c4111a1ab58d318448c8
tarekyou <tarekyoussef88@hotmail.com>
2022-03-05 14:04:52 -0800

    indexed db for tutors

--- client/src/components/TutorDisplay/index.js
+++ client/src/components/TutorDisplay/index.js
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 import Tutor from "../Tutor";
-import { Row, Col} from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { QUERY_TUTORS } from "../../utils/queries";
 import { useQuery } from "@apollo/client";
+import { idbPromise } from "../../utils/helpers";
 
 const TutorDisplay = () => {
   const [query, setQuery] = useState("");
 
   const { loading, data } = useQuery(QUERY_TUTORS);
+  // console.log(data);
+  // console.log(data.tutors);
+  if (data) {
+    // console.log(data.tutors[0]);
+    data.tutors.forEach((tutor) => {
+      idbPromise("tutors", "get", tutor);
+    });
+  }
 
+  // data.tutors.forEach((tutor) => {
+  //   idbPromise("tutors", "get", tutor);
+  // });
 
   if (loading) {
     return <div>Loading...</div>;
@@ -16,39 +28,45 @@ const TutorDisplay = () => {
 
   return (
     <>
-    <Row className="mt-4 p-4 justify-content-center">
-    <h1 className='display-2 p-4' style={{fontWeight: '400'}}>
-      The <b style={{color: 'WindowText', fontFamily: ''}}>first place</b> to look when you study.
-    </h1>
+      <Row className="mt-4 p-4 justify-content-center">
+        <h1 className="display-2 p-4" style={{ fontWeight: "400" }}>
+          The <b style={{ color: "WindowText", fontFamily: "" }}>first place</b>{" "}
+          to look when you study.
+        </h1>
 
-       <input className="col-lg-8 p-3" style={{borderRadius: '20px', backgroundColor: 'blanchedalmond', borderStyle: 'hidden' }}
-        placeholder='Enter search term'
-        onChange={(event) => setQuery(event.target.value)}
-      /></Row>
-     
-    <Row className="mt-4">
-      {data.tutors
-        .filter((tutor) => {
-          if (query === "") {
-            return tutor;
-          } else if (
-            tutor.userId.email.toLowerCase().includes(query.toLowerCase()) ||
-            tutor.userId.username.toLowerCase().includes(query.toLowerCase())
-          ) {
-            return tutor;
-          }
-        })
-        .map((tutor, index) => {
-          return (
-            
-            <Col key={index} sm={4}>
-              <Tutor tutor={tutor}></Tutor>
+        <input
+          className="col-lg-8 p-3"
+          style={{
+            borderRadius: "20px",
+            backgroundColor: "blanchedalmond",
+            borderStyle: "hidden",
+          }}
+          placeholder="Enter search term"
+          onChange={(event) => setQuery(event.target.value)}
+        />
+      </Row>
+
+      <Row className="mt-4">
+        {data.tutors
+          .filter((tutor) => {
+            if (query === "") {
+              return tutor;
+            } else if (
+              tutor.userId.email.toLowerCase().includes(query.toLowerCase()) ||
+              tutor.userId.username.toLowerCase().includes(query.toLowerCase())
+            ) {
+              return tutor;
+            }
+          })
+          .map((tutor, index) => {
+            return (
+              <Col key={index} sm={4}>
+                <Tutor tutor={tutor}></Tutor>
               </Col>
-          );
-        })}
-    </Row>
-    
-  </>
+            );
+          })}
+      </Row>
+    </>
   );
 };
 

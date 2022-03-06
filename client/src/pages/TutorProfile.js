fbce0cce048b0051c09380925ce9fa29f8e83665
tarekyou <tarekyoussef88@hotmail.com>
2022-03-06 12:40:19 -0800

    indexeddb for chat and tutor

--- client/src/pages/TutorProfile.js
+++ client/src/pages/TutorProfile.js
@@ -66,111 +66,12 @@ function TutorProfile() {
     return <div>Loading...</div>;
   }
 
-<<<<<<< HEAD
-  function AlertDismissibleExample() {
-    const [show, setShow] = useState(true);
-
-    if (show) {
-      return (
-        <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-          <Alert.Heading>Hey! You got a new message!</Alert.Heading>
-        </Alert>
-      );
-    }
-    return <Button onClick={() => setShow(true)}>Show Messages</Button>;
-  }
-
-  return (
-    <>
-      <Row className="mt-4">
-        <Col>
-          <img
-            src={data.me.user.photo}
-            alt="profile"
-            style={{
-              width: "400px",
-              borderRadius: "100%",
-              height: "400px",
-              boxShadow: "8px 8px 8px gray",
-            }}
-          />
-        </Col>
-        <Col className="mt-4">
-          <div>
-            <h2>Your Dashboard</h2>
-          </div>
-          <div>
-            <div>
-              <p>
-                <b>Name:</b> {data.me.user.username}
-              </p>
-              <p>
-                <b>Email Address:</b> {data.me.user.email}
-              </p>
-              <p>
-                <b>Hourly rate: $</b>
-                {data.me.tutor.hourlyRate}
-              </p>
-              <p>
-                <b>Expertises:</b> {data.me.tutor.knownSubjects}
-              </p>
-              <p>
-                <b>Bio:</b> {data.me.tutor.bio}
-              </p>
-
-              {/* <p>{data.me.tutor._id}</p> */}
-            </div>
-          </div>
-          <div>
-            <h3>Messages</h3>
-            <AlertDismissibleExample />
-          </div>
-          <div>
-            <h3>Student Lists</h3>
-          </div>
-        </Col>
-        <Form.Group>
-          <Card className="p-4" style={{ width: "28rem" }}>
-            <h3>Update Username and Password</h3>
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className="form-input m-2"
-                placeholder="username"
-                name="username"
-                type="username"
-                id="username"
-                value={formState.username}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="******"
-                name="password"
-                type="password"
-                id="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              {/* {errorMessage && (
-            <div>
-              <p className="error-text">{errorMessage}</p>
-            </div>
-          )} */}
-              <Button className="btn d-block" variant="primary" type="submit">
-                Submit
-              </Button>
-            </form>
-          </Card>
-        </Form.Group>
-      </Row>
-    </>
-=======
   if (!Auth.loggedIn()) {
     return <div>Not logged in</div>;
   }
 
   return (
-    <Row className='mt-4'>
+    <Row className="mt-4">
       <Col sm={6}>
         <Card>
           <Card.Header>
@@ -184,62 +85,61 @@ function TutorProfile() {
                   src={data.me.user.photo}
                   fluid={true}
                 ></Image>
-                
               </Col>
               <Col sm={6}>
                 <Form onSubmit={saveUpdates}>
-                  <Form.Group className='mb-2'>
+                  <Form.Group className="mb-2">
                     <Form.Label>Username</Form.Label>
                     <Form.Control
                       disabled={!formEditable}
-                      name='username'
-                      type='text'
+                      name="username"
+                      type="text"
                       value={formState.username}
                       onChange={handleChange}
                     />
                   </Form.Group>
-                  <Form.Group className='mb-2'>
+                  <Form.Group className="mb-2">
                     <Form.Label>Email</Form.Label>
                     <Form.Control
-                      as='input'
+                      as="input"
                       disabled={true}
-                      name='email'
+                      name="email"
                       value={formState.email}
                       onChange={handleChange}
                     />
                   </Form.Group>
-                  <Form.Group className='mb-2'>
+                  <Form.Group className="mb-2">
                     <Form.Label>Hourly Rate</Form.Label>
                     <Form.Control
-                      as='input'
+                      as="input"
                       disabled={!formEditable}
-                      name='hourlyRate'
+                      name="hourlyRate"
                       value={formState.hourlyRate}
                       onChange={handleChange}
                     />
                   </Form.Group>
-                  <Form.Group className='mb-2'>
+                  <Form.Group className="mb-2">
                     <Form.Label>Skills</Form.Label>
                     <Form.Control
-                      as='input'
+                      as="input"
                       disabled={!formEditable}
-                      name='knownSubjects'
+                      name="knownSubjects"
                       value={formState.knownSubjects}
                       onChange={handleChange}
                     />
                   </Form.Group>
-                  <Form.Group className='mb-2'>
+                  <Form.Group className="mb-2">
                     <Form.Label>Bio</Form.Label>
                     <Form.Control
-                      as='textarea'
-                      name='bio'
+                      as="textarea"
+                      name="bio"
                       disabled={!formEditable}
                       value={formState.bio}
                       onChange={handleChange}
                     />
                   </Form.Group>
                   <Button
-                    className='mb-2'
+                    className="mb-2"
                     variant={formEditable ? "danger" : "primary"}
                     onClick={formEditable ? saveUpdates : allowUpdates}
                   >
@@ -247,12 +147,12 @@ function TutorProfile() {
                   </Button>
                 </Form>
                 <Form>
-                  <Form.Group className='mb-2'>
+                  <Form.Group className="mb-2">
                     <Form.Label>Update Password</Form.Label>
                     <Form.Control
-                      as='input'
-                      name='password'
-                      type='password'
+                      as="input"
+                      name="password"
+                      type="password"
                       onChange={handleChange}
                     />
                   </Form.Group>
@@ -290,7 +190,6 @@ function TutorProfile() {
         </Card>
       </Col>
     </Row>
->>>>>>> f2a17c1269ad2d4302b81f06c12a5c641126a0dd
   );
 }
 

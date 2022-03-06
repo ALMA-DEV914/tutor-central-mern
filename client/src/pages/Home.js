599f9c02dd183f70018e68c156a34eb76034b158
tbellenger <tbellenger@gmail.com>
2022-03-06 10:29:36 -0800

    styling on tutor display

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,56 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Auth from "../utils/auth";
 import TutorDisplay from "../components/TutorDisplay";
- import { Container, Col, Modal, Button} from "react-bootstrap";
+import { Container, Col, Modal, Button } from "react-bootstrap";
 import StudentSignup from "./StudentSignup";
-
 
 const Home = (props) => {
   const [show, setShow] = useState(false);
-  
-const handleClose = () => setShow(false);
-const handleShow = () => setShow(true);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   if (props.logout) {
     Auth.logout();
   }
 
-
-return (
+  return (
     <>
-
-<Container fluid className='text-center'>
-      <Col sm={8} style={{margin: '8% auto'}}>
-     <h1 className="mt-4 p-2">
-     The first place to look when you study.
-     </h1>
-     <p className="p-2">Tutor-Connect is an on-demand marketplace for top Web development engineers, developers, consultants, architects, programmers, and tutors. Get your projects built by vetted Web development freelancers or learn from expert mentors with team training & coaching experiences.</p>
-     <Modal show={show} onHide={handleClose}>
+      <Col sm={8} className='text-center mx-auto mt-5'>
+        <h1 className='p-3'>The first place to look when you study</h1>
+        <p className='p-2'>
+          Tutor Central is a freelance marketplace for top developers to provide
+          tutoring services. Learn from tutors and get your problems solved by
+          experienced development freelancers.
+        </p>
+        <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
             <Modal.Title>Welcome on board!</Modal.Title>
           </Modal.Header>
-          <Modal.Body>{<StudentSignup/>}</Modal.Body>
+          <Modal.Body>{<StudentSignup />}</Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
+            <Button variant='secondary' onClick={handleClose}>
               Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
             </Button>
           </Modal.Footer>
         </Modal>
-     <Button variant="danger" onClick={handleShow}>
-         GET HELP NOW
-     </Button>
-     </Col>
-    </Container>
-  
-   <hr style={{heigth: '2px grey'}}>
-     
-   </hr>
-    <TutorDisplay />
-  
+        {!Auth.loggedIn && (
+          <Button variant='danger' onClick={handleShow}>
+            GET HELP NOW
+          </Button>
+        )}
+      </Col>
+
+      <hr />
+      <TutorDisplay />
     </>
   );
 };
